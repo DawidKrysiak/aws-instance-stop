e4d29349809e3cf8218283c114037570d428a2fa
GrendelPL <58400812+GrendelPL@users.noreply.github.com>
2021-06-14 21:21:36 +0100

    Initial commit
    Just basic functionality (ec2+one tag)

---
+++ instance-stop.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+
+    region = os.environ['AWS_REGION']
+    ec2 = boto3.client('ec2', region_name=region)
+
+    reservations = ec2.describe_instances().get('Reservations', [])
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+        ], [])
+
+    for instance in instances:
+        try:
+            for tag in instance['Tags']:
+                if tag['Key'] == 'uptime':
+                    if tag['Value'] == 'daytime':
+                        instance = instance['InstanceId']
+                        response = ec2.stop_instances(InstanceIds=[instance])
+                        print("stopping instance", instance)
+        except:
+            print("no instances")
